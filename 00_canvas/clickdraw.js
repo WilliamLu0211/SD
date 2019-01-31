e9634c9f60ca03ada8a75190393a02815909bb4e
William Lu <rlu1@stuy.edu>
2019-01-31 13:23:15 -0500

    :

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -2,11 +2,12 @@ var c = document.getElementById("slate");
 var w = c.getAttribute("width");
 var h = c.getAttribute("height");
 var ctx = c.getContext("2d");
-
+ctx.fillStyle = "blue";
 var rect = true;
 
 var clr = document.getElementById("clr");
-clr.addEventListener('click', function() {
+clr.addEventListener('click', function(e) {
+  e.preventDefault();
   ctx.clearRect(0, 0, w, h);
 })
 
@@ -16,23 +17,28 @@ tgl.addEventListener('click', function(){
   rect = !rect;
   if (rect){
     this.innerHTML = "rectangle mode";
+    this.setAttribute("class", "btn btn-primary");
   }
   else {
     this.innerHTML = "dot mode";
+    this.setAttribute("class", "btn btn-success");
   }
 })
 
-c.addEventListener('click', function(e){
-  var r = c.getBoundingClientRect();
-  console.log(r);
-  var x = e.clientX - r.left;
-  var y = e.clientY - r.top;
-  if (rect){
-    ctx.fillRect(x - 10, y - 10, 20, 20);
-  }
-  else {
-    ctx.beginPath();
-    ctx.ellipse(x, y, 5, 5, 0, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-})
+c.addEventListener('click', function(e) {
+  // e.preventDefault();
+  if (rect)
+    drawBox(e);
+  else
+    drawDot(e);
+});
+
+var drawBox = function(e) {
+  ctx.fillRect(e.offsetX - 10, e.offsetY - 10, 20, 20);
+}
+
+var drawDot = function(e) {
+  ctx.beginPath();
+  ctx.ellipse(e.offsetX, e.offsetY, 5, 5, 0, 0, 2 * Math.PI);
+  ctx.fill();
+}
